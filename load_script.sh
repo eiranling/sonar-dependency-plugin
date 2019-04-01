c332c107b7b21871c5972ad55b4639a5f85f10c4
Eiran Ling <eli26@uclive.ac.nz>
2019-04-01 14:43:41 +1300

    Update load_script.sh

--- load_script.sh
+++ load_script.sh
@@ -1,3 +1,2 @@
-mvn clean package
-cp ./target/sonar-dependency-plugin-1.0.jar ~/SonarQube/sonarqube-7.6/extensions/plugins/
-~/SonarQube/sonarqube-7.6/bin/linux-x86-64/sonar.sh restart
+cp ./target/sonar-dependency-plugin-1.0.jar /opt/sonarqube/extensions/plugins/
+/opt/sonarqube/bin/linux-x86-64/sonar.sh restart
