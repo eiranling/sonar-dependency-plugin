fd1ad001d6522cdbde3dbed6c1f31b9f1b851054
Eiran Ling <eiranling@gmail.com>
2019-04-17 19:37:57 +1200

    Implemented list endpoint.

--- src/main/java/org/sonarsource/plugins/dependencies/webapp/RequestHandlers/ListDependenciesHandler.java
+++ src/main/java/org/sonarsource/plugins/dependencies/webapp/RequestHandlers/ListDependenciesHandler.java
@@ -1,10 +1,15 @@
 package org.sonarsource.plugins.dependencies.webapp.RequestHandlers;
 
+import com.google.common.collect.Lists;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.RequestHandler;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.server.ws.WebService;
 import org.sonar.api.utils.text.JsonWriter;
+import org.sonarqube.ws.Measures;
+import org.sonarqube.ws.client.WsClient;
+import org.sonarqube.ws.client.WsClientFactories;
+import org.sonarqube.ws.client.measures.ComponentTreeRequest;
 
 public class ListDependenciesHandler implements RequestHandler {
 
@@ -29,8 +34,30 @@ public class ListDependenciesHandler implements RequestHandler {
 
     @Override
     public void handle(Request request, Response response) {
-        response.newJsonWriter().beginObject()
+        WsClient client = WsClientFactories.getLocal().newClient(request.localConnector());
+
+        ComponentTreeRequest treeRequest = new ComponentTreeRequest()
+                .setComponent(request.getParam("baseComponentKey").getValue())
+                .setMetricKeys(Lists.newArrayList("dependencies"));
+
+        Measures.ComponentTreeWsResponse treeResponse = client.measures().componentTree(treeRequest);
+
+        JsonWriter writer = response.newJsonWriter().beginObject()
                 .name("dependencies")
-                .beginArray().endArray().endObject().close();
+                .beginArray();
+
+        for (Measures.Component component : treeResponse.getComponentsList()) {
+            writer.beginObject()
+                    .name("component")
+                    .prop("componentId", component.getId())
+                    .prop("componentKey", component.getKey())
+                    .prop("name", component.getName());
+            for (Measures.Measure measure : component.getMeasuresList()) {
+                writer.prop(measure.getMetric(), measure.getValue());
+            }
+            writer.endObject();
+        }
+
+        writer.endArray().endObject().close();
     }
 }
