eeb6b1a2b4fefb2a4f0fc76439c9bdc58ca3e915
Eiran Ling <eiranling@gmail.com>
2019-04-17 14:29:51 +1200

    Testing adding a server side annotation to ClientFactory

--- src/main/java/org/sonarsource/plugins/dependencies/webapp/util/ClientFactory.java
+++ src/main/java/org/sonarsource/plugins/dependencies/webapp/util/ClientFactory.java
@@ -3,10 +3,11 @@ package org.sonarsource.plugins.dependencies.webapp.util;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
+import org.sonar.api.server.ServerSide;
 
 import java.io.IOException;
 
-
+@ServerSide
 public class ClientFactory {
     public static OkHttpClient generate() {
         return new OkHttpClient.Builder().build();
