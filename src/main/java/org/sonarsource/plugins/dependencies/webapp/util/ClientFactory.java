58311a3c2d21ac8cd0af8c3f20861cf10fbf67fc
Eiran Ling <eiranling@gmail.com>
2019-04-17 14:11:29 +1200

    Added client factory

--- src/main/java/org/sonarsource/plugins/dependencies/webapp/util/ClientFactory.java
+++ src/main/java/org/sonarsource/plugins/dependencies/webapp/util/ClientFactory.java
@@ -1,7 +1,14 @@
 package org.sonarsource.plugins.dependencies.webapp.util;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 
 public class ClientFactory {
-
+    public static OkHttpClient generate() {
+        return new OkHttpClient.Builder().build();
+    }
 }
