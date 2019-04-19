b8f27fcd9bbef788f4360631a0c54c252261221f
Eiran Ling <eiranling@gmail.com>
2019-04-19 16:31:27 +1200

    Test

--- src/main/js/app-dependency_graph.js
+++ src/main/js/app-dependency_graph.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
 import DependencyGraph from "./components/DependencyGraph";
+import VersionsMeasuresHistoryApp from "./components/VersionsMeasuresHistoryApp";
 import './style.css';
 
 window.registerExtension("dependencies/graph", options => {
@@ -8,7 +9,7 @@ window.registerExtension("dependencies/graph", options => {
     const { el } = options;
 
     render(
-        <DependencyGraph project={options.component} />, el
+        <VersionsMeasuresHistoryApp project={options.component} />, el
     );
 
     return () => unmountComponentAtNode(el);
