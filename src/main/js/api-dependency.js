11c7a7283ab8f8439083e8cf87fec3175f9ff923
Eiran Ling <eiranling@gmail.com>
2019-04-19 21:04:15 +1200

    test

--- src/main/js/api-dependency.js
+++ src/main/js/api-dependency.js
@@ -1,12 +1,12 @@
 import {getJSON} from 'sonar-request';
 
 export function getAllDependencies(project) {
+    console.log(project);
     return getJSON('/api/dependencies/list', {
         baseComponentKey: project,
         p: 1,
         ps: 500
     }).then((response) => {
-        console.log(response);
         return response.dependencies;
     })
 }
