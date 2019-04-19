ef60700aef595b242a486fbfe4b1f58ec6b6b00c
Eiran Ling <eiranling@gmail.com>
2019-04-19 16:19:41 +1200

    Test

--- src/main/js/components/DependencyGraph.js
+++ src/main/js/components/DependencyGraph.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import { render } from 'react-dom';
 import Graph from 'react-d3-graph';
 
 export default class DependencyGraph extends React.PureComponent {
 
-    state = {
-        graph: {
-            nodes: [
-                { id: "Controller" },
-                { id: "Model" },
-                { id: "View" }
-            ],
-            links: [
-                {source: "Controller", target:"Model"},
-                {source: "View", target: "Controller"},
-                {source: "Model", target: "View"}
+    constructor(props) {
+        super(props);
+        this.state = {
+            graph: {
+                nodes: [
+                    {id: "Controller"},
+                    {id: "Model"},
+                    {id: "View"}
+                ],
+                links: [
+                    {source: "Controller", target: "Model"},
+                    {source: "View", target: "Controller"},
+                    {source: "Model", target: "View"}
+                ]
+            },
+            config: [
+                {}
             ]
-        },
-        config: [
-            {}
-        ]
-    };
+        };
+    }
 
     render() {
+
+        function changeState() {
+            const nodes = this.state.graph.nodes.slice();
+            nodes.concat({id: "hello"});
+            this.state.graph.nodes = nodes;
+        }
+
         return (
             <div className="page page-limited">
                 <Graph id='dep-graph' data={this.state.graph} config={this.state.config}/>
-                <button onClick={this.state.graph.nodes.concat({ id: "New"})}>Refresh</button>
+                <button onClick={changeState}>Refresh</button>
             </div>
         );
     };
