33a7b6bdd862dc85139719276b65ee1c76b1f175
Eiran Ling <eiranling@gmail.com>
2019-04-19 17:05:55 +1200

    Test

--- src/main/js/app-dependency_graph.js
+++ src/main/js/app-dependency_graph.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from "react-dom";
-import DependencyGraph from "./components/DependencyGraph";
-import VersionsMeasuresHistoryApp from "./components/VersionsMeasuresHistoryApp";
+import { render, unmountComponentAtNode } from 'react-dom';
+import VersionsMeasuresHistoryApp from './components/VersionsMeasuresHistoryApp';
 import './style.css';
 
 window.registerExtension('dependencies/graph', options => {
