e31e6870679a9fd6f4de18ba92ed8b3a6d5a6da8
Eiran Ling <eiranling@gmail.com>
2019-04-19 13:39:54 +1200

    Added stuff.

--- src/main/js/app-dependency_graph.js
+++ src/main/js/app-dependency_graph.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
+import DependencyGraph from "./components/DependencyGraph";
 
 window.registerExtension("dependencies/graph", options => {
 
--- src/main/js/components/DependencyGraph.js
+++ src/main/js/components/DependencyGraph.js
@@ -2,21 +2,33 @@ import React from 'react';
 import { render } from 'react-dom';
 import Graph from 'react-d3-graph';
 
-class DependencyGraph extends React.PureComponent {
+export default class DependencyGraph extends React.PureComponent {
 
     constructor(props) {
         super(props);
         this.state = {
-            graph: [
-                { }
-            ],
+            graph: {
+                nodes: [
+                    { id: "Controller" },
+                    { id: "Model" },
+                    { id: "View" }
+                ],
+                links: [
+                    {source: "Controller", target:"Model"},
+                    {source: "View", target: "Controller"},
+                    {source: "Model", target: "View"}
+                ]
+            },
+            config: [
+                {}
+            ]
         }
     }
 
     render() {
         return (
             <div id="dependencyGraph">
-                <Graph graph={graph} options={options} events={events}/>
+                <Graph id="depGraph" graph={this.state.graph} config={this.state.config}/>
             </div>
         );
     };
