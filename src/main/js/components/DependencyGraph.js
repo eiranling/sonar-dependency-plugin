c95c4c6d464736b9f5a934f8971eef6dea01c58a
Eiran Ling <eiranling@gmail.com>
2019-04-19 20:00:12 +1200

    test

--- src/main/js/components/DependencyGraph.js
+++ src/main/js/components/DependencyGraph.js
@@ -39,12 +39,13 @@ export default class DependencyGraph extends React.PureComponent {
             let edgeList = [];
             destinations.forEach((item) => {
                 edgeList.concat([{ from: from, to: item }])
-            })
+            });
             return edgeList;
         }
 
         getAllDependencies(this.props.project).then((valuesReturned) => {
             valuesReturned.forEach((component) => {
+                console.log(component);
                const nodes = this.state.graph.nodes.slice();
                const edges = this.state.graph.edges.slice();
                this.state.graph = {
