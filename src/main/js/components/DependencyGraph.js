ae3c4a352c838b297dbfe2fb1b0decf8b0a20aca
Eiran Ling <eiranling@gmail.com>
2019-04-19 15:49:16 +1200

    fixed graph

--- src/main/js/components/DependencyGraph.js
+++ src/main/js/components/DependencyGraph.js
@@ -4,30 +4,27 @@ import Graph from 'react-d3-graph';
 
 export default class DependencyGraph extends React.PureComponent {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            graph: {
-                nodes: [
-                    { id: "Controller" },
-                    { id: "Model" },
-                    { id: "View" }
-                ],
-                links: [
-                    {source: "Controller", target:"Model"},
-                    {source: "View", target: "Controller"},
-                    {source: "Model", target: "View"}
-                ]
-            },
-            config: [
-                {}
+    state = {
+        graph: {
+            nodes: [
+                { id: "Controller" },
+                { id: "Model" },
+                { id: "View" }
+            ],
+            links: [
+                {source: "Controller", target:"Model"},
+                {source: "View", target: "Controller"},
+                {source: "Model", target: "View"}
             ]
-        }
-    }
+        },
+        config: [
+            {}
+        ]
+    };
 
     render() {
         return (
-            <div id="dependencyGraph" className="page page-limited sanity-check">
+            <div className="page page-limited">
                 <Graph id="depGraph" data={this.state.graph} config={this.state.config}/>
             </div>
         );
