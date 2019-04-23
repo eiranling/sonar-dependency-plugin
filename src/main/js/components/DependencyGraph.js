330985016e8875989c8720add8e80b12fe5d21be
Eiran Ling <eiranling@gmail.com>
2019-04-23 17:06:11 +1200

    test

--- src/main/js/components/DependencyGraph.js
+++ src/main/js/components/DependencyGraph.js
@@ -88,13 +88,14 @@ export default class DependencyGraph extends React.PureComponent {
                 valuesReturned.forEach((component) => {
                     if (component.declared_classes !== undefined) {
                         let declared_classes = component.declared_classes.split(';');
-                        new_edges.map((edge) => {
+                        new_edges = new_edges.map((edge) => {
                             if (declared_classes.includes(edge.from)) {
                                 edge.from = component.componentKey;
                             }
                             if (declared_classes.includes(edge.to)) {
                                 edge.to = component.componentKey;
                             }
+                            return edge;
                         });
                     }
                 });
